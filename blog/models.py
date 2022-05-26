ab733eef273b0b635abb31bb198ee3bbe01227dc
Akbarshokh <akbarshokh2001@gmail.com>
2022-05-26 10:23:05 +0500

    2nd Chapter Finished

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,13 @@
-import email
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset()\
-            .filter(status='published')
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -21,8 +19,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250,
                             unique_for_date='publish')
     author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -31,18 +29,9 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
-    objects = models.Manager() # The deafault manager
-    published = PublishedManager() # My custom manager
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
     tags = TaggableManager()
-
-    def get_absolute_url(self):
-        return reverse("blog:post_detail",
-                       args=[
-                           self.publish.year,
-                           self.publish.month,
-                           self.publish.day,
-                           self.slug
-                       ])
 
     class Meta:
         ordering = ('-publish',)
@@ -50,9 +39,17 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -64,4 +61,4 @@ class Comment(models.Model):
         ordering = ('created',)
 
     def __str__(self):
-        return f'Comment by  {self.name} on {self.post}'
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
 from django.core.mail import send_mail
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -14,30 +16,33 @@ def post_list(request, tag_slug=None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-         # If page is not an integer deliver the first page
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page,
-                                                    'posts': posts,
-                                                    'tag': tag})
-        
+    return render(request,
+                 'blog/post/list.html',
+                 {'page': page,
+                  'posts': posts,
+                  'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                             status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+
     # List of active comments for this post
-    comments =post.comments.filter(active=True)
+    comments = post.comments.filter(active=True)
 
     new_comment = None
 
@@ -47,25 +52,27 @@ def post_detail(request, year, month, day, post):
         if comment_form.is_valid():
             # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
-            #Assign the current post to the comment
+            # Assign the current post to the comment
             new_comment.post = post
             # Save the comment to the database
             new_comment.save()
     else:
         comment_form = CommentForm()
+
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids)\
                                   .exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-                                 .order_by('-same_tags','-publish')[:4]
+                                .order_by('-same_tags','-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
                    'comment_form': comment_form,
-                   'similiar_posts':similar_posts,})
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -76,30 +83,25 @@ class PostListView(ListView):
 
 
 def post_share(request, post_id):
-    #retrieve post by id
+    # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
     if request.method == 'POST':
-        #Form was submitted
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            #form fields passed validation
+            # Form fields passed validation
             cd = form.cleaned_data
-            #... send email
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url())
-            subject = f"{cd['name']} recommends you read " \
-                f"{post.title}"
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
-                f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'akbarshox1823@gmail.com',
-                      [cd['to']])
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
+
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {
-        'post': post,
-        'form': form,
-        'sent': sent,
-    })
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
